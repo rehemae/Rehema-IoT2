daa80a8878629ec0f4554433f134e5ce2aa635bd
rehemae <99467501+rehemae@users.noreply.github.com>
2022-03-11 09:25:26 +0300

    Add files via upload

---
+++ terrific_jofo1 (1).ino
@@ -0,0 +1,20 @@
+// C++ code
+//
+
+void setup()
+{
+  pinMode(1, OUTPUT);
+  pinMode(0,OUTPUT);
+}
+
+void loop()
+{
+  digitalWrite(1, HIGH);
+  delay(1000); // Wait for 1000 millisecond(s)
+  digitalWrite(1, LOW);
+  delay(1000); // Wait for 1000 millisecond(s)
+  digitalWrite(0,HIGH);
+  delay(1000);
+  digitalWrite(0,LOW);
+  delay(1000);
+}
